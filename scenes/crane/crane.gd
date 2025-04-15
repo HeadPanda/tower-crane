e432d28a10b47032d3d20bf40bdd4588865c1290
HeadPanda <171028156+HeadPanda@users.noreply.github.com>
2025-04-14 22:22:02 -0500

    Working crane controls

--- scenes/crane/crane.gd
+++ scenes/crane/crane.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 @export var acceleration := 100.0
 @export var friction := 900
 @export var top_offset := 8
+@export var rotation_amount := 1.0
 
 @export_group("Chains")
 @export var chain_softness: float = 0.03
@@ -14,6 +15,7 @@ extends CharacterBody2D
 var direction : Vector2
 var held_block : RigidBody2D
 var joint: PinJoint2D
+
 @onready var grab_area: Area2D = %GrabArea
 @onready var hook: RigidBody2D = %Hook
 @onready var chains: Node2D = $ChainContainer/Chains
@@ -30,6 +32,14 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("attach"):
 		_on_grab_pressed()
+	
+	if held_block:
+		if Input.is_action_pressed("rotate_left"):
+			rotate_around_hook(-rotation_amount)
+		elif Input.is_action_pressed("rotate_right"):
+			rotate_around_hook(rotation_amount)
+		elif Input.is_action_just_released("rotate_left") or Input.is_action_just_released("rotate_right"):
+			held_block.lock_rotation = false
 
 
 func apply_movement(delta: float):
@@ -50,21 +60,38 @@ func apply_movement(delta: float):
 	move_and_slide()
 
 
+func rotate_around_hook(angle_deg: float):
+	var pivot = hook.global_position
+	var dir = held_block.global_position - pivot
+	held_block.lock_rotation = true
+	dir = dir.rotated(deg_to_rad(angle_deg))
+	held_block.global_position = pivot + dir
+	held_block.rotation += deg_to_rad(angle_deg)
+
+
 func _on_grab_pressed():
 	if held_block:
 		# Drop the block
 		if joint:
 			joint.queue_free()
 			joint = null
+		held_block.set_collision_layer_value(3, true)
+		held_block.set_collision_layer_value(4, false)
+		held_block.set_collision_mask_value(2, true)
 		held_block = null
 	else:
 		for body in grab_area.get_overlapping_bodies():
 			if body.is_in_group("Blocks"):
 				held_block = body
+				held_block.set_collision_layer_value(3, false)
+				held_block.set_collision_layer_value(4, true)
+				held_block.set_collision_mask_value(2, false)
+				#held_block.lock_rotation = true
+				
 				# Create the joint
 				joint = PinJoint2D.new()
 				joint.softness = 0.02
-				joint.bias = 0.5
+				joint.bias = 0.4
 				joint.position = Vector2.ZERO
 				joint.node_a = hook.get_path()
 				joint.node_b = held_block.get_path()
