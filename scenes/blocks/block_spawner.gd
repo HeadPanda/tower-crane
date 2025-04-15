7633b1ad64205606a0f3c3cdca8829b9aec7396d
HeadPanda <171028156+HeadPanda@users.noreply.github.com>
2025-04-15 01:30:33 -0500

    Adjust spawn delays

--- scenes/blocks/block_spawner.gd
+++ scenes/blocks/block_spawner.gd
@@ -11,15 +11,18 @@ extends Node2D
 
 
 func _ready() -> void:
-	spawn_timer.wait_time = spawn_delay + randf_range(-1.0, spawn_delay_variance)
+	spawn_timer.wait_time = spawn_delay + randf_range(-spawn_delay, spawn_delay_variance)
 	spawn_timer.start()
 
 func _on_spawn_timer_timeout() -> void:
-	spawn_timer.wait_time = spawn_delay + randf_range(0, spawn_delay_variance)
+	spawn_timer.wait_time = spawn_delay + randf_range(-1.0, spawn_delay_variance)
 	await get_tree().physics_frame
 	if area_2d.has_overlapping_bodies() or area_2d.has_overlapping_areas():
 		return
-	
+	spawn()
+
+
+func spawn() -> void:
 	var random_block: RigidBody2D = block_types.pick_random().instantiate()
 	
 	if len(rotations) > 0:
@@ -27,3 +30,4 @@ func _on_spawn_timer_timeout() -> void:
 	
 	random_block.global_position = global_position
 	blocks_container.add_child(random_block)
+	
