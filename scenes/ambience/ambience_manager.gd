b81593635bc0625f6ac6a741e96d98017ecf9186
HeadPanda <171028156+HeadPanda@users.noreply.github.com>
2025-04-16 01:26:24 -0500

    Start working on ambience manager

---
+++ scenes/ambience/ambience_manager.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+@export var ambient_tracks_day : Array[AudioStream]
+@export var ambient_tracks_night : Array[AudioStream]
+@export var independent_random_ambient_tracks : Array[AudioStream]
